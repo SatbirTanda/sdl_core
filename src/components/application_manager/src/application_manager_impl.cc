36bfd5d3ae1a51ba43371aa3b296f86513ec34b9
Andriy Byzhynar <AByzhynar@luxoft.com>
2018-01-29 15:22:19 +0200

    Fix A/V streaming starting for PROJECTION apps

--- src/components/application_manager/src/application_manager_impl.cc
+++ src/components/application_manager/src/application_manager_impl.cc
@@ -3957,6 +3957,8 @@ mobile_apis::AppHMIType::eType ApplicationManagerImpl::StringToAppHMIType(
     return mobile_apis::AppHMIType::MESSAGING;
   } else if ("NAVIGATION" == str) {
     return mobile_apis::AppHMIType::NAVIGATION;
+  } else if ("PROJECTION" == str) {
+    return mobile_apis::AppHMIType::PROJECTION;
   } else if ("INFORMATION" == str) {
     return mobile_apis::AppHMIType::INFORMATION;
   } else if ("SOCIAL" == str) {
